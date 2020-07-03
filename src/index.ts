d0b727d8b749179300de6dc911855ff5a325d6cc
whatasoda <git@whatasoda.me>
2020-07-03 13:29:03 +0900

    fix: fix export values in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { extractPrimitives } from './extract-primitives';
-module.exports = Object.assign(extractPrimitives, exports);
 export default extractPrimitives;
 export * from './extract-primitives';
